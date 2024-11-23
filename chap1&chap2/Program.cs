efc8d26e2d3612dbf66e9f9a39e9e7ee648423f6
StechClarin <stechclarin@gmail.com>
2024-11-23 08:32:22 +0000

    Ajoutez des fichiers projet.

---
+++ chap1&chap2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class JeuNombre
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Choisissez un nombre entre 1 et 10 : ");
+            int nombre = int.Parse(Console.ReadLine());
+
+            if (nombre < 1 || nombre > 10)
+            {
+                throw new ArgumentException("Saisissez un nombre compris entre [1, 10].");
+            }
+
+            if (nombre == 5)
+            {
+                Console.WriteLine("Vous avez gagné !");
+            }
+            else
+            {
+                Console.WriteLine("Vous avez perdu.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur : {ex.Message}");
+        }
+    }
+}
