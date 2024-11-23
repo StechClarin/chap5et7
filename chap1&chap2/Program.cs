836e0fe2ecdcde83808deefe919eb2d0ac4462c1
StechClarin <stechclarin@gmail.com>
2024-11-23 08:37:20 +0000

    etape3

--- chap1&chap2/Program.cs
+++ chap1&chap2/Program.cs
@@ -5,32 +5,49 @@ class JeuNombre
 {
     static void Main(string[] args)
     {
-        List<int> choixFaits = new List<int>();
-        bool jeuEnCours = true;
+        Console.WriteLine("Entrez la borne minimale : ");
+        int borneMin = int.Parse(Console.ReadLine());
 
-        while (jeuEnCours)
+        Console.WriteLine("Entrez la borne maximale : ");
+        int borneMax = int.Parse(Console.ReadLine());
+
+        if (borneMin >= borneMax)
+        {
+            Console.WriteLine("Les bornes sont incorrectes. Réessayez.");
+            return;
+        }
+
+        Random rnd = new Random();
+        int nombreCible = rnd.Next(borneMin, borneMax + 1);
+        List<int> choixFaits = new List<int>();
+        int tentatives = 0;
+
+        Console.WriteLine($"Trouvez le nombre entre {borneMin} et {borneMax} : ");
+
+        while (true)
         {
             try
             {
-                Console.WriteLine("Choisissez un nombre entre 1 et 10 : ");
                 int nombre = int.Parse(Console.ReadLine());
+                tentatives++;
 
-                if (nombre < 1 || nombre > 10)
+                if (nombre < borneMin || nombre > borneMax)
                 {
-                    throw new ArgumentException("Saisissez un nombre compris entre [1, 10].");
+                    throw new ArgumentException($"Saisissez un nombre compris entre {borneMin} et {borneMax}.");
                 }
 
                 choixFaits.Add(nombre);
 
-                if (nombre == 5)
+                if (nombre == nombreCible)
                 {
-                    Console.WriteLine("Vous avez gagné !");
-                    jeuEnCours = false;
+                    Console.WriteLine("Félicitations ! Vous avez trouvé le nombre.");
+                    double note = (double)(borneMax - borneMin + 1) / tentatives;
+                    Console.WriteLine($"Votre note est : {note:F2}");
+                    break;
                 }
                 else
                 {
-                    Console.WriteLine("Vous avez perdu.");
-                    Console.WriteLine($"Choix précédents : {string.Join(", ", choixFaits)}");
+                    Console.WriteLine("Incorrect. Réessayez !");
                 }
             }
             catch (Exception ex)
