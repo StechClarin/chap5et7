b4f1877fe760c11ede1b3caf80d03c4a45e9b082
StechClarin <stechclarin@gmail.com>
2024-11-23 10:33:35 +0000

    Ajoutez des fichiers projet.

---
+++ UsingCollectionsDemo/Etudiant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsageCollections
+{
+    public class Étudiant
+    {
+        public string Nom { get; set; }
+        public double NoteCC { get; set; } // Note de contrôle continu
+        public double NoteDevoir { get; set; } // Note des devoirs
+
+        // Calcul de la moyenne pondérée
+        public double CalculerMoyenne()
+        {
+            return (NoteCC * 0.33) + (NoteDevoir * 0.67);
+        }
+    }
+}
