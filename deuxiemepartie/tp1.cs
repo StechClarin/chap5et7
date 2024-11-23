8267487e262ba4828776145bb4590ec07ad9c01f
StechClarin <stechclarin@gmail.com>
2024-11-23 09:04:01 +0000

    debut tp1 deuxieme partie

---
+++ deuxiemepartie/tp1.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class ConversionHexa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entrez un entier à convertir en hexadécimal :");
+        int entier = int.Parse(Console.ReadLine());
+
+        string hexValue = ConvertirEnHexadecimal(entier);
+        Console.WriteLine($"La valeur hexadécimale de {entier} est : {hexValue}");
+    }
+
+    static string ConvertirEnHexadecimal(int nombre)
+    {
+        // Implémentation ici
+        return "";
+    }
+}
+
